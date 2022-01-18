5c9f5be799af411e1ea258db110a2c3cf5736fb3
huaweiwei <harvey0379@163.com>
2022-01-19 00:16:11 +0800

    crud

--- mybatis-demo/src/main/java/com/huauauaa/dao/UserMapper.java
+++ mybatis-demo/src/main/java/com/huauauaa/dao/UserMapper.java
@@ -2,13 +2,20 @@ package com.huauauaa.dao;
 
 import com.huauauaa.pojo.User;
 import java.util.List;
+import java.util.Map;
 
 public interface UserMapper {
 
-    /**
-     * get users
-     *
-     * @return
-     */
     List<User> getUserList();
+
+    User getUserById(int id);
+
+    int addUser(User user);
+
+    int addUserViaMap(Map<String, Object> map);
+
+    int updateUser(User user);
+
+    int deleteUser(int id);
+
 }
--- mybatis-demo/src/test/java/com/huauauaa/dao/UserMapperTest.java
+++ mybatis-demo/src/test/java/com/huauauaa/dao/UserMapperTest.java
@@ -2,23 +2,80 @@ package com.huauauaa.dao;
 
 import com.huauauaa.pojo.User;
 import com.huauauaa.utils.MybatisUtils;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.apache.ibatis.session.SqlSession;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class UserMapperTest {
 
-    SqlSession sqlSession = MybatisUtils.getSqlSession();
+    private SqlSession sqlSession;
+    private UserMapper userMapper;
 
-    // option1
-    UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-    List<User> userList = userMapper.getUserList();
-    
-    // option2
-//    List<User> userList = sqlSession.selectList("com.huauauaa.dao.UserMapper.getUserList");
+    @Before
+    public void before() {
+        sqlSession = MybatisUtils.getSqlSession();
+        userMapper = sqlSession.getMapper(UserMapper.class);
+    }
+
+
+    @After
+    public void after() {
+        sqlSession.close();
+    }
 
     @Test
-    public void test() {
+    public void testSelect() {
+
+        List<User> userList = userMapper.getUserList();
+
+        // option2
+//    List<User> userList = sqlSession.selectList("com.huauauaa.dao.UserMapper.getUserList");
         userList.forEach(System.out::println);
     }
+
+    @Test
+    public void testSelectOne() {
+        User user = userMapper.getUserById(0);
+        User user1 = userMapper.getUserById(1);
+
+        Assert.assertNotNull(user);
+        Assert.assertEquals(user1, null);
+    }
+
+
+    @Test
+    public void testAdd() {
+        User user = new User(1, "Harvey", "123");
+        userMapper.addUser(user);
+        sqlSession.commit();
+    }
+
+    @Test
+    public void testAddViaMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("userId", 2);
+        map.put("username", "Martin");
+        userMapper.addUserViaMap(map);
+        sqlSession.commit();
+    }
+
+    @Test
+    public void testUpdate() {
+        User user = new User(4, "bqq88qb", "bb");
+        Integer result = userMapper.updateUser(user);
+        System.out.println(result);
+        sqlSession.commit();
+    }
+
+    @Test
+    public void testDelete() {
+        userMapper.deleteUser(2);
+        sqlSession.commit();
+    }
+
 }
